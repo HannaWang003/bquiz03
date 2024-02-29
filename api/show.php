33f9b9bd208fc6509e71be95bd120b458637af61
HannaWang003 <hannawang003@gmail.com>
2024-02-29 21:38:27 +0800

    practice3-init

--- api/show.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "db.php";
-$row = $Movie->find($_POST['id']);
-// $row['sh'] = ($row['sh'] == 1) ? 0 : 1;
-// 運行效果: 運算式會比判斷式快
-$row['sh'] = ($row['sh']+1)%2;
-$Movie->save($row);

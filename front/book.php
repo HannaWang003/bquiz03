9d486296627f20611a646a86438f51213547b801
HannaWang003 <hannawang003@gmail.com>
2024-03-02 20:23:38 +0800

    完成訂購

--- front/book.php
+++ front/book.php
@@ -1,6 +1,6 @@
 <?php
 include_once "../api/db.php";
-$row=$Movie->find($_POST['movie'])['name'];
+$movie=$Movie->find($_POST['movie'])['name'];
 $date=$_POST['date'];
 $sess=$_POST['sess'];
 ?>
@@ -54,11 +54,40 @@ for($i=1;$i<=20;$i++){
     </div>
 </div>
 <div>
-    <div id="movie">您選擇的電影是: <span class="red"><?=$row?></span></div>
-    <div>您選擇的時刻是:<span id="date" class='red'><?=$_POST['date']?></span>&nbsp;&nbsp;<span id="sess" class='red'><?=$_POST['sess']?></span></div>
+    <div id="movie">您選擇的電影是: <span class="red"><?=$movie?></span></div>
+    <div>您選擇的時刻是:<span id="date" class='red'><?=$date?></span>&nbsp;&nbsp;<span id="sess" class='red'><?=$sess?></span></div>
     <div>你已勾選了<span id="num" class='red'></span>張票，最多可以購買<span  class='red'>四</span>張票</div>
 </div>
 <div>
     <button onclick="$('#select').show();$('#book').hide()">上一步</button>
-    <button>訂購</button>
+    <button onclick="checkout()">訂購</button>
 </div>
+<script>
+    let seats=new Array();
+    $('.chk').on("change",function(){
+        if($(this).prop('checked')){
+            if(seats.length+1<=4){
+                seats.push($(this).val())
+            }else{
+                $(this).prop('checked',false);
+                alert("每個人只能勾選四張票")
+            }
+            
+        }else{
+            seats.splice(seats.indexOf($(this).val()),1)
+        }
+        console.log(seats.length)
+        $('#num').text(seats.length);
+    })
+    function checkout(){
+        $.post("../api/checkout.php",{
+            movie:'<?=$movie?>',
+            date:'<?=$date?>',
+            session:'<?=$sess?>',
+            seats},
+            (no)=>{
+                // console.log(no);
+                location.href=`?do=result&no=${no}`;
+            })
+    }
+</script>

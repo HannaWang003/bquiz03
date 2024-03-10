71fd86a57a269c028ce67519da38b43a64dcba22
HannaWang003 <hannawang003@gmail.com>
2024-03-10 18:35:58 +0800

    練習輪播程式

--- front/main.php
+++ front/main.php
@@ -1,105 +1,97 @@
 <style>
-  .lists{
-    text-align:center;
-    position:relative;
-    height:300px;
-    overflow:hidden;
-  }
-  .list{
-    display:none;
-    width:200px;
-    margin:auto;
-    display:none;
-  }
-  .list *{
-   box-sizing:border-box;
-  }
-  .list img{
-    width:100%;
-  }
-  .controls{
-    width:420px;
-    height:100px;
-    display:flex;
-    align-items:center;
-    position:relative;
-  }
-  .btns{
-    width:360px;
-    height:100px;
-    display:flex;
-    overflow:hidden;
-    margin:auto;
-  }
-  .btn{
-    width:90px;
-    text-align:center;
-    font-size:12px;
-    flex-shrink:0;
-    position:relative;
-  }
-  .btn img{
-    width:60px;
-    height:80px;
-  }
-  .left,.right{
-    width:0px;
-    border:20px solid #000;
-    border-top-color:transparent;
-    border-bottom-color:transparent;
-  }
-  .left{
-    border-left-width:0;
-  }
-  .right{
-    border-right-width:0;
-  }
+.lists{
+  width:430px;
+  height:300px;
+  overflow:hidden;
+  position:relative;
+}
+.list{
+  width:250px;
+  display:none;
+  margin:auto;
+}
+.list img{
+  width:100%;
+}
+.btns{
+  display:flex;
+  overflow:hidden;
+  width:360px;
+  height:100px;
+  margin:auto;
+  position:relative;
+}
+.btn{
+  width:90px;
+  height:100px;
+  text-align:center;
+  flex-shrink:0;
+  position:relative;
+}
+.btn img{
+  width:60px;
+}
+.right,.left{
+  width:0;
+  border:20px solid #000;
+  border-top-color: transparent;
+  border-bottom-color: transparent;
+}
+.right{
+  border-right-width: 0;
+}
+.left{
+  border-left-width: 0;
+}
+.controls{
+  display:flex;
+  align-items:center;
+}
 </style>
 <div class="half" style="vertical-align:top;">
       <h1>預告片介紹</h1>
       <div class="rb tab" style="width:95%;">
         <div id="abgne-block-20111227">
-          <div class="lists">
-<?php
-$posters = $Poster->all(['sh'=>1],"order by rank");
-foreach($posters as $idx=> $poster){
-  ?>
-<div class="list" data-ani="<?=$poster['ani']?>">
-  <img src="./img/<?=$poster['img']?>" alt="">
-  <div class="ct"><?=$poster['text']?></div>
-</div>
+<div class="lists">
   <?php
-}
-?>         
-          </div>
-
-          <div class="controls">
-            <div class="left"></div>
-            <div class="btns">
-<?php
+$posters = $Poster->all(['sh'=>1],"order by rank");
 foreach($posters as $idx=>$poster){
   ?>
-<div class="btn">
-  <img src="./img/<?=$poster['img']?>" alt="">
-  <div class="ct"><?=$poster['text']?></div>
+  <div class="list" data-ani="<?=$poster['ani']?>" >
+    <img src="./img/<?=$poster['img']?>" alt="">
+    <div class="ct"><?=$poster['text']?></div>
 </div>
   <?php
 }
 ?>
 </div>
-<div class="right"></div>
+          <div class="controls">
+            <div class="left"></div>
+            <div class="btns">
+              <?php
+              foreach($posters as $idx=>$poster){
+  ?>
+              <div class="btn">
+ <img src="./img/<?=$poster['img']?>" alt="">
+<div class="ct"><?=$poster['text']?></div>
+              </div>
+              <?php
+              }
+              ?>
+            </div>
+            <div class="right"></div>
           </div>
         </div>
       </div>
     </div>
 <script>
   $('.list').eq(0).show();
-  let total=$(".btn").length;
-  //輪播
+  let total = $('.btn').length;
 let now=0;
 let next=0;
-let timer = setInterval(slide,3000);
-function slide(n){
+let timer=setInterval(slider,3000);
+function slider(n){
+  let ani=$('.list').eq(now).data('ani');
   if(typeof(n)=="undefined"){
     next=now+1;
     if(next>=total){
@@ -108,58 +100,54 @@ function slide(n){
   }else{
     next=n;
   }
-  let ani = $('.list').eq(now).data('ani');
   switch(ani){
     case 1:
-      $('.list').eq(now).fadeOut(1500,function(){
-        $('.list').eq(next).fadeIn(1500)
+      $('.list').eq(now).fadeOut(1000,function(){
+        $('.list').eq(next).fadeIn(1000)
       })
       break;
-      case 2:
-        $('.list').eq(now).hide(1500,function(){
-          $('.list').eq(next).show(1500)
-        })
-        break;
-      case 3:
-        $('.list').eq(now).slideUp(1500,function(){
-          $('.list').eq(next).slideDown(1500)
-        })
-        break;
+    case 2:
+      $('.list').eq(now).hide(1000,function(){
+        $('.list').eq(next).show(1000)
+      })
+      break;
+    case 3:
+      $('.list').eq(now).slideUp(1000,function(){
+        $('.list').eq(next).slideDown(1000)
+      })
+      break;
   }
   now=next;
 }
 
 
-
-
-
-  //按鈕
-  let p=0;
-  $(".left,.right").on('click',function(){
-    let arrow = $(this).attr('class');
-    switch(arrow){
-      case "right":
-        if(p+1<=(total-4)){
-          p++;
-        }
-        break;
-        case "left":
-          if(p-1>=0){
-            p--;
-          }
-          break;
-    }
-    $('.btn').animate({right:90*p});
-    $('.btn').on('click',function(){
-      let idx=$(this).index();
-      slide(idx);
-    })
-    $('.btns').hover(function(){
-      clearInterval(timer);
-    },function(){
-      timer=setInterval(slide,3000)
-    })
-  })
+let p=0;
+$('.left,.right').on('click',function(){
+  let arrow = $(this).attr('class');
+  switch(arrow){
+    case "left":
+      if(p-1>=0){
+      p--;
+      }
+      break;
+    case "right":
+      if(p+1<=total-4){
+        p++
+      }
+      break;
+  }
+  $('.btn').animate({right:90*p});
+})
+$('.btns').hover(function(){
+  clearInterval(timer)
+},
+function(){
+  timer = setInterval(slider,3000)
+})
+$('.btn').on('click',function(){
+  let idx = $(this).index();
+  setInterval(slider(idx),3000)
+})
 </script>
 
     <div class="half">
